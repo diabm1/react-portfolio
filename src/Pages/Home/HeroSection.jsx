b659a8a83a3614731f36507ee7b00b825f280863
diabm1 <diabm1855@gmail.com>
2024-07-23 08:36:42 -0400

    finished navbar and hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import heroImage from "../Home/img/hero_img.png";
 
 export default function HeroSection() {
-  return <h1>HeroSection</h1>;
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm John</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src={heroImage} alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
