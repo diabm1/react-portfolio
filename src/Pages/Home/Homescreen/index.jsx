deffedaa1a24bb3aef95d0e5c62daba98f012e81
diabm1 <diabm1855@gmail.com>
2024-07-24 19:26:42 -0400

    finished my skills section

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../HeroSection";
+import MySkills from "../MySkills";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <MySkills />
     </>
   );
 }
